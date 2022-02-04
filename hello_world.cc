15f0607b01efc8c5a93675e7c2958534c44e87a0
nehakundra20 <nkundra@stevens.edu>
2022-02-04 15:50:36 -0500

    added a for loop

--- hello_world.cc
+++ hello_world.cc
@@ -1,4 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main() { cout << "hello world \n"; }
+int main() {
+  cout << "hello world \n";
+  for (int i = 0; i <= 10; i++) {
+    cout << i << " ";
+  }
+}
